4e76c37860f4a6bd6e98709a18d939908ca2f615
baseem <baseem@gmail.com>
2018-10-26 14:30:50 -1000

    project update

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    lat: 21.436057,
+    lon: -157.788867,
+    status: 'Looking for location...'
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(location => {
+      this.setState({
+        lat: location.coords.latitude,
+        lon: location.coords.longitude,
+        status: 'Location found...updating server'
+      })
+    })
+    setInterval(() => {
+      this.sendPoi()
+    },1000)
+}
+
+
+  sendPoi = () => {
+    console.log(this.state);
+    axios.get(`/api/gps/${this.state.lat}/${this.state.lon}`)
+    .then(res=> {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log('err');
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h2>{this.state.status}</h2>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cowsay = require('cowsay')
+const cors = require('cors')
+const path = require('path')
+
+// Create the server
+const app = express()
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+
+let coords = [21.436057,-157.788867];
+// Serve our api route /cow that returns a custom talking text cow
+app.get('/api/gps/:lat/:lon', cors(), async (req, res, next) => {
+  console.log(req.params);
+  let lat = req.params.lat;
+  let lon = req.params.lon;
+  coords[0] = lat;
+  coords[1] = lon;
+  res.json(coords);
+})
+
+app.get('/api/coords', (req,res) => {
+	res.json(coords);
+})
+
+
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`)
+})
